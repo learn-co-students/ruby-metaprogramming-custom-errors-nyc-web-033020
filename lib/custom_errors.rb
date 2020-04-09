75f50eba90ad2618cc09d807395a159841efb9d8
coralfussman <coralfussman@gmail.com>
2020-04-09 19:54:53 -0400

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -1,21 +1,23 @@
-class Person
-  attr_accessor :partner, :name
-
-  def initialize(name)
-    @name = name
+  class Person
+    attr_accessor :partner, :name
+   
+    def initialize(name)
+      @name = name
+    end
+   
+    def get_married(person)
+      self.partner = person
+      if person.class != Person
+        raise PartnerError
+      else
+        person.partner = self
+      end
+    end
+   
+    class PartnerError < StandardError
+    end
   end
-
-  def get_married(person)
-    self.partner = person
-    person.partner = self
-  end
-
-end
-
-beyonce = Person.new("Beyonce")
-beyonce.get_married("Jay-Z")
-puts beyonce.name
-
-
-
-
+   
+  beyonce = Person.new("Beyonce")
+  beyonce.get_married("Jay-Z")
+  puts beyonce.name
