68a3991054caf4e0a366399a79c2f6c50a76b85b
lightfire224 <elias.taveras@columbia.edu>
2020-04-10 07:28:31 -0400

    Done.

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -7,11 +7,20 @@ class Person
 
   def get_married(person)
     self.partner = person
-    person.partner = self
+    if person.class != Person
+      raise PartnerError
+    else
+      person.partner = self
   end
+end
 
 end
 
+  class PartnerError < StandardError
+    def message "must have an instance of the person class passed in!"
+    end
+  end
+end
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
